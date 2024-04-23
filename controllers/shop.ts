08500d37e95b690d7ac464852529babee9cf3f0c
Riccardo <CX23-1@slc.net>
2024-04-23 12:31:38 +0700

    feat: crud shop done

--- controllers/shop.ts
+++ controllers/shop.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import Shop from "../models/shop";
+import ProductVariant from "../models/product-variant";
+import Product from "../models/product";
 
 export function create_shop(req: Request, res: Response, next: NextFunction) {
     const errors = validationResult(req)
@@ -12,9 +14,7 @@ export function create_shop(req: Request, res: Response, next: NextFunction) {
         // error.data = errors.array()
         // throw error
     }
-    const name = req.body.name
-    const description = req.body.description
-    const user_id = req.body.user_id
+    const {name, description, user_id} = req.body
     const shop = new Shop({
         name: name,
         description: description,
@@ -33,6 +33,73 @@ export function create_shop(req: Request, res: Response, next: NextFunction) {
 
 }
 
-export function get_shop(req: Request, res: Response, next: NextFunction) {
+export async function get_shops(req: Request, res: Response, next: NextFunction){
+    try {
+        const shops = await Shop.find().populate({
+            path: 'products',
+            populate: {
+                path: 'product_variants'
+            }
+        })
+        return res.status(200).json({shops: shops})
+    } catch (error) {
+        return res.status(500).json({error: "Error fetching shops"})
+    }
+}
 
+export async function get_shop(req: Request, res: Response, next: NextFunction) {
+    const {id} = req.params
+    try {
+        const shop = await (await Shop.findById(id)).populate({
+            path: 'products',
+            populate: {
+                path: 'product_variants'
+            }
+        })
+        if(!shop){
+            return res.status(404).json({error: "Shop not found"})
+        }
+        return res.status(200).json({shop: shop})
+    } catch (error) {
+        console.log(`Error fetching shop: `, error)
+        return res.status(500).json({error: "Error fetching shop"})
+    }
+}
+
+export async function update_shop(req: Request, res: Response, next: NextFunction){
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+    }
+    const {name, description, user_id} = req.body
+    const {id} = req.params
+    try {
+        const shop = await Shop.findById(id)
+        if(!shop){
+            return res.status(404).json({error: "Shop not found"})
+        }
+        shop.name = name
+        shop.description = description
+        await shop.save()
+        return res.status(200).json({shop: shop})
+    } catch (error) {
+        return res.status(500).json({error: "Error updating shop"})
+    }
+}
+
+export async function delete_shop(req: Request, res: Response, next: NextFunction){
+    const {id} = req.params
+    try {
+        const shop = await Shop.findByIdAndDelete(id)
+        if(!shop){
+            return res.status(404).json({error: "Shop not found"})
+        }
+        for(const product of shop.products){
+            await Product.deleteOne({_id: product._id})
+            await ProductVariant.deleteMany({product: product._id})
+        }
+        return res.status(200).json({message: "Delete shop success"})
+    } catch (error) {
+        return res.status(500).json({error: "Error deleting shop"})
+    }
 }
--- routes/shop.ts
+++ routes/shop.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { create_shop } from "../controllers/shop";
+import { create_shop, delete_shop, get_shop, get_shops } from "../controllers/shop";
 import User from "../models/user";
 import Shop from "../models/shop";
+import { delete_products, update_products } from "../controllers/product";
 
 const router = Router()
 
-router.post('/shop', [
+const validate_shop = [
     body('name')
         .trim()
         .notEmpty()
@@ -29,6 +30,21 @@ router.post('/shop', [
                 return Promise.reject("User has already created a shop")
             }
         })
+]
+
+router.post('/shop', [
+    ...validate_shop
 ], create_shop)
+
+router.get('/shops', get_shops)
+
+router.get('/shop/:id', get_shop)
+
+
+router.put('/shop/:id', [
+    ...validate_shop
+], update_products)
+
+router.delete('/shop/:id', delete_shop)
 
 export default router

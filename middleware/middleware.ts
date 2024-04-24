d4476e00be75ae3b0a949474d20d0758a273baec
Riccardo <CX23-1@slc.net>
2024-04-24 09:15:53 +0700

    fix: middleware validate token fixed

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -1,22 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
+import { IGetUserAuthInfoRequest } from "../interfaces/get-user-auth-info-request";
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY || "JWT_SECRET_KEY"
 
-export function validate_token(req: Request, res: Response, next: NextFunction){
-    const authHeader = req.get("authorization")
-    if(!authHeader){
-        return false
-    }
-    const token = authHeader.split(' ')[1]
-    let decodedToken: {user_id: string}
-    try {
-        decodedToken = jwt.verify(token, SECRET_KEY) as {user_id: string}
-    } catch (error) {
-        return next()
-    }
-    if(!decodedToken){
-        return next()
-    }
-    next()
+export function validate_token(req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, SECRET_KEY as string, (err: any, user: any) => {
+        console.log(err)
+
+        if (err) return res.sendStatus(403)
+
+        req.user = user
+
+        next()
+    })
 }
